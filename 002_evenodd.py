07b95b5fbcd888c06a200b9d3e33302707e3fa48
Atheeq Mohamed <atheeq.mohamed2k5@gmail.com>
2024-08-21 20:13:29 +0530

    Printing the type of the variable

--- 002_evenodd.py
+++ 002_evenodd.py
@@ -3,8 +3,10 @@
 # this statement is to get input from the user
 input_num = input("Enter a number:")
 
+print(type(input_num))
 # all user inputs are by default strings. This statement is to convert string to integer
 input_num = int(input_num)
+print(type(input_num))
 
 # this statement checks if the number is divisible by 2
 if input_num % 2 == 0:
